b3d17ce2b9c809ee3e9535e6d31386247a01158f
IntPtr <lxy.lixiaoyan@gmail.com>
2017-02-18 20:27:59 +0800

    add react-helmet

--- entry/server.js
+++ entry/server.js
@@ -2,6 +2,7 @@ import Koa from "koa";
 import React from "react";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router";
+import Helmet from "react-helmet";
 
 export default app => ({ config, manifest }) => {
   const server = new Koa();
@@ -15,26 +16,30 @@ export default app => ({ config, manifest }) => {
       </StaticRouter>
     ));
 
+    const head = Helmet.rewind();
+
     if (router.url) {
       ctx.redirect(router.url);
       return;
     }
 
     ctx.body = `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="UTF-8" />
-          <title></title>
-          <link rel="stylesheet" href="${manifest["app.css"]}" />
-        </head>
-        <body>
-          <div id="app">${content}</div>
-          <script src="${manifest["app.js"]}"></script>
-          <script>main.default();</script>
-        </body>
-      </html>
-    `;
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    ${head.title.toString()}
+    <link rel="stylesheet" href="${manifest["app.css"]}" />
+  </head>
+  <body>
+    <div id="app">${content}</div>
+    <script src="${manifest["app.js"]}"></script>
+    <script>main.default();</script>
+  </body>
+</html>
+
+    `.trim();
   });
 
   server.listen(config.bind.server.port, config.bind.server.host);

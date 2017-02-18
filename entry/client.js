209a9235e27334fd3a6c81984c9291ed0c4ad11e
IntPtr <lxy.lixiaoyan@gmail.com>
2017-02-18 20:10:43 +0800

    add react-router

--- entry/client.js
+++ entry/client.js
@@ -1,5 +1,11 @@
+import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter } from "react-router";
 
 export default app => () => {
-  render(app(), document.querySelector("#app"));
+  render((
+    <BrowserRouter>
+      {app()}
+    </BrowserRouter>
+  ), document.querySelector("#app"));
 };
--- entry/server.js
+++ entry/server.js
@@ -1,11 +1,26 @@
-import express from "express";
+import Koa from "koa";
+import React from "react";
 import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router";
 
 export default app => ({ config, manifest }) => {
-  const server = express();
+  const server = new Koa();
 
-  server.use((req, res) => {
-    res.send(`
+  server.use((ctx) => {
+    const router = {};
+
+    const content = renderToString((
+      <StaticRouter location={ctx.url} context={router}>
+        {app()}
+      </StaticRouter>
+    ));
+
+    if (router.url) {
+      ctx.redirect(router.url);
+      return;
+    }
+
+    ctx.body = `
       <!DOCTYPE html>
       <html>
         <head>
@@ -14,12 +29,12 @@ export default app => ({ config, manifest }) => {
           <link rel="stylesheet" href="${manifest["app.css"]}" />
         </head>
         <body>
-          <div id="app">${renderToString(app())}</div>
+          <div id="app">${content}</div>
           <script src="${manifest["app.js"]}"></script>
           <script>main.default();</script>
         </body>
       </html>
-    `);
+    `;
   });
 
   server.listen(config.bind.server.port, config.bind.server.host);
